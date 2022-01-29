910b394552a22b6279e85ff42fd44317b899b4eb
Ariel <arielox.ag@gmail.com>
2022-01-29 04:53:01 -0300

    inicial listo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
+const sendEmail = require('./controllers/sendEmail');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -12,8 +12,9 @@ app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
-  res.send('<h1>Email Project</h1>');
+   res.send('<h1>Email Project</h1> <a href="/send">Send emilio</a>');
 });
+app.get('/send', sendEmail);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -21,13 +22,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+   try {
+      app.listen(port, () =>
+         console.log(`Server is listening on port ${port}...`)
+      );
+   } catch (error) {
+      console.log(error);
+   }
 };
 
 start();
